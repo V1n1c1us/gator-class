c78809c27aec099de0db7110fdc41fb8e267408e
Vinicius de Franceschi <EIGI\vfranceschi@brfl-2876-l.corp.endurance.com>
2022-01-03 18:15:51 -0300

    Wip

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,12 +1,7 @@
-import PageStyle from './Page.style';
 import { Card } from '@src/components';
 
 const Home = () => {
-  return (
-    <PageStyle.Container>
-      <Card description="Si u mundo tá muito paradis? Toma um mé que o mundo vai girarzis!" />
-    </PageStyle.Container>
-  );
-}
+  return <Card description="Si u mundo tá muito paradis? Toma um mé que o mundo vai girarzis!" />;
+};
 
 export default Home;
