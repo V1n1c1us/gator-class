4a59690de18936f218c7079e0dc7c15a327a2f4f
Vinicius de Franceschi <EIGI\vfranceschi@brfl-2876-l.corp.endurance.com>
2022-01-03 23:21:03 -0300

    GitHub action setup

--- src/components/Button/Button.style.tsx
+++ src/components/Button/Button.style.tsx
@@ -7,6 +7,12 @@ export const Button = styled.button`
     border: none;
     color: ${theme.colors.gradient.primary};
     padding: 10px;
+    transition: 0.5s;
     width: 100%;
+    font-size: ${theme.font.sizes.md};
+
+    &:hover {
+      background: ${theme.colors.gray};
+    }
   `}
 `;
