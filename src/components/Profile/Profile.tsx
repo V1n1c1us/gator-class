4d23f823b1842b83dfec9a7269b82f00c6f5bf2f
Vinicius de Franceschi <EIGI\vfranceschi@brfl-2876-l.corp.endurance.com>
2022-01-04 11:53:39 -0300

    Profile refactored

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,4 +1,5 @@
 import Lottie from 'react-lottie';
+import { useMemo, useState } from 'react';
 import avatar1 from '../../../assets/images/1.json';
 import avatar2 from '../../../assets/images/2.json';
 import avatar3 from '../../../assets/images/3.json';
@@ -6,24 +7,28 @@ import avatar4 from '../../../assets/images/4.json';
 import * as Style from './Profile.style';
 import { ProfileProps } from './Profile.types';
 import { Button } from '@src/components';
-
 const Profile = ({
   description = 'Default Description',
   buttonLabel = 'Default Label',
   subTitle = 'Default Subscription',
   title = 'Default Title',
 }: ProfileProps) => {
-  const dataAnimation = [avatar1, avatar2, avatar3, avatar4];
-  const avatarRandom = Math.floor(Math.random() * (3 - 0 + 1) + 0);
+  const [avatarRandom] = useState(Math.floor(Math.random() * (3 - 0 + 1) + 0));
 
-  const defaultOptions = {
-    loop: true,
-    autoplay: true,
-    animationData: dataAnimation[avatarRandom],
-    rendererSettings: {
-      preserveAspectRatio: 'xMidYMid slice',
-    },
-  };
+  const dataAnimation = useMemo(() => {
+    return [avatar1, avatar2, avatar3, avatar4];
+  }, []);
+
+  const defaultOptions = useMemo(() => {
+    return {
+      loop: true,
+      autoplay: true,
+      animationData: dataAnimation[avatarRandom],
+      rendererSettings: {
+        preserveAspectRatio: 'xMidYMid slice',
+      },
+    };
+  }, [avatarRandom, dataAnimation]);
 
   return (
     <Style.Container>
